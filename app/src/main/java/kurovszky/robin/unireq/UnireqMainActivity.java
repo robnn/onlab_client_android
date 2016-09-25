88c9cb85a9f8ac2d5c6bd6a2108161746c4f5658
Robin <robin.kurovszky@gmail.com>
2016-09-25 13:44:42 +0200

    initial commit

---
+++ app/src/main/java/kurovszky/robin/unireq/UnireqMainActivity.java
@@ -0,0 +1,13 @@
+package kurovszky.robin.unireq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UnireqMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_unireq_main);
+    }
+}
